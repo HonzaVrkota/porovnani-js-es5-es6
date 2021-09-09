409e4793729120cdf6229c48f468b292eab0137a
Honza Vrkota <vrkota.jan@hotmail.com>
2021-09-09 15:06:54 +0200

    first commit

---
+++ es5.js
@@ -0,0 +1,15 @@
+// Zápis standardem ES5
+
+
+
+/**
+ * Funkce pro sečtení dovu hodnot 5 + 5
+ * 
+ * @returns Vrátí hodnotu součtu 5 + 5 
+ */
+function sum() {
+  return 5 + 5;
+}
+
+// Vypíše hodnotu součtu funkce do konzole
+console.log(sum());
---
+++ es6.js
@@ -0,0 +1,11 @@
+// Zápis standardem ES6
+
+/**
+ * Funkce pro sečtení dovu hodnot 5 + 5
+ * 
+ * @returns Vrátí hodnotu součtu 5 + 5 
+ */
+const sum = () => 5 + 5;
+
+// Vypíše hodnotu součtu funkce do konzole
+console.log(`Součet z konzole od ES6 je: ${sum()}`);
